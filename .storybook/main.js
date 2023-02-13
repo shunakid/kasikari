ce163fbb81d573943c053922ed462eb96c3753ca
shunakid <shunakid@icloud.com>
2023-02-13 23:59:23 +0900

    アドオンをmain.jsに追加、webpackFinalの設定は必要なアドオンを導入しtsconfigの設定を引き継ぐため

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -6,4 +9,21 @@ module.exports = {
     '@storybook/addon-postcss',
   ],
   staticDirs: ['public'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+    ]
+
+    return config
+  },
 }
